53b0760be1705d29e0d925797da885f7efe733a8
Le-Michael88 <le.michael88@gmail.com>
2019-09-14 12:48:17 -0400

    Starting UI

--- app/src/main/java/com/example/driveralertness/MainActivity.java
+++ app/src/main/java/com/example/driveralertness/MainActivity.java
@@ -3,7 +3,8 @@ package com.example.driveralertness;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +12,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mainscreen);
+
+        Button startButton = findViewById(R.id.startbutton);
+        Button infoButton = findViewById(R.id.information);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+            }
+        });
     }
 
 }
